b55db7b1e03376f2281f0872823c412044710927
JovanRuzic <ruzicjovan96@gmail.com>
2019-12-19 10:01:44 +0100

    Umotvorine finished for now

--- js/index.js
+++ js/index.js
@@ -5,13 +5,41 @@ let umotvorineElement = document.querySelector(".umotvorine"),
         "Ko drugome jamu kopa, sam u nju upada.",
         "Ko laže za tebe, lagaće i protiv tebe.",
         "Ko mnogo priča, ili sve zna, ili sve laže."
-    ];
+    ],
+    lastUmotvorinaIndex;
 
 function updateUmotvorina() {
-    let umotvorinaIndex = Math.round(Math.random() * 4);
+    let umotvorinaIndex = getRandomUmotvorinaIndex();
+    lastUmotvorinaIndex = umotvorinaIndex;
 
-    console.log(umotvorinaIndex);
-    umotvorineElement.innerHTML = umotvorine[umotvorinaIndex];
+    umotvorineElement.innerHTML = `<h1 class="umotvorine-heading"><span id="word"></span> <span class="typing-indicator"></span></h1>`;
+
+    writeUmotvorina(umotvorine[umotvorinaIndex]);
+}
+
+function writeUmotvorina(wordToWrite) {
+    let letters = wordToWrite.split(""),
+        intervalTime = 8000 / letters.length,
+        index = 0;
+
+    let writingInterval = setInterval(() => {
+        document.querySelector('#word').innerHTML += letters[index];
+        index++;
+
+        if (index === letters.length) {
+            clearInterval(writingInterval);
+        }
+    }, intervalTime);
+}
+
+function getRandomUmotvorinaIndex() {
+    let index;
+
+    do {
+        index = Math.round(Math.random() * 4);
+    } while (index === lastUmotvorinaIndex);
+
+    return index;
 }
 
 updateUmotvorina();

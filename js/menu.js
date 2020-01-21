18da9cecfaf7437a2237866a64aee7718d8c394d
JovanRuzic <ruzicjovan96@gmail.com>
2020-01-21 15:54:00 +0100

    All done for submission

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,68 @@
-let menu = document.querySelector('.menu'),
-    menuSheets = document.querySelector('.menu-sheets');
+let activeSheet = 0,
+    nextPageControllers = document.querySelectorAll('.menu-page.front .flip-page'),
+    previousPageControllers = document.querySelectorAll('.menu-page.back .flip-page');
+
+nextPageControllers.forEach(controller => {
+    controller.addEventListener('click', flipToNextPage);
+});
+
+previousPageControllers.forEach(controller => {
+    controller.addEventListener('click', flipToPreviousPage);
+});
+
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'ArrowLeft') {
+        flipToPreviousPage();
+    } else if (e.key === 'ArrowRight') {
+        flipToNextPage();
+    }
+});
 
 function flipToNextPage() {
+    let allSheets = document.querySelectorAll('.menu-sheet'),
+        pagesFlipping = document.querySelectorAll('.flipping').length;
 
+    if (activeSheet >= allSheets.length - 2 || pagesFlipping) {
+        return false;
+    }
+
+    allSheets[activeSheet].classList.remove('active');
+
+    activeSheet += 1;
+
+    allSheets[activeSheet].classList.remove('right');
+    allSheets[activeSheet].classList.add('flipping');
+    allSheets[activeSheet].classList.add('left');
+
+    allSheets[activeSheet+1].classList.add('active');
+
+    setTimeout(function () {
+        allSheets[activeSheet].classList.remove('flipping');
+    }, 500);
 }
 
 function flipToPreviousPage() {
+    let allSheets = document.querySelectorAll('.menu-sheet'),
+        pagesFlipping = document.querySelectorAll('.flipping').length;
 
+    if (activeSheet <= 0 || pagesFlipping) {
+        return false;
+    }
+
+    activeSheet -= 1;
+
+    allSheets[activeSheet + 2].classList.remove('active');
+    // allSheets[activeSheet + 2].style.zIndex = '3';
+
+    allSheets[activeSheet + 1].classList.remove('left');
+    allSheets[activeSheet + 1].classList.add('flipping');
+    allSheets[activeSheet + 1].classList.add('right');
+
+    allSheets[activeSheet].classList.add('active');
+
+    setTimeout(function () {
+        allSheets[activeSheet + 1].classList.remove('flipping');
+        // allSheets[activeSheet + 2].style.zIndex = null;
+    }, 500);
 }
--- js/ordering.js
+++ js/ordering.js
@@ -1,15 +1,140 @@
 let orderForm = document.querySelector('#order-form'),
     buildingCheckbox = document.querySelector('#isBuilding'),
-    closeMenuButton = document.querySelector('.close-modal');
+    closeMenuButton = document.querySelector('.close-modal'),
+    orderFood = document.querySelector('#finish-choosing'),
+    allMeals = document.querySelectorAll('.meal'),
+    mealsData = [
+        {
+            name: "Sendvič sa piletinom",
+            price: 4.60
+        },
+        {
+            name: "Sendvič sa kačkavaljem",
+            price: 3.25
+        },
+        {
+            name: "Pizza",
+            price: 4.60
+        },
+        {
+            name: "Biftek",
+            price: 36.90
+        },
+        {
+            name: "Sushi",
+            price: 10.45
+        },
+        {
+            name: "Pasta",
+            price: 4.95
+        },
+        {
+            name: "Supa",
+            price: 3.45
+        },
+        {
+            name: "Burger",
+            price: 10.00
+        },
+        {
+            name: "Engleski doručak",
+            price: 13.90
+        },
+        {
+            name: "Američke palačinke",
+            price: 8.45
+        },
+        {
+            name: "Čokoladni kolač",
+            price: 2.15
+        },
+        {
+            name: "Cheesecake",
+            price: 2.95
+        },
+    ],
+    chosenMeals = [];
 
+orderFood.addEventListener('click', openOrderForm);
 orderForm.addEventListener('submit', processOrderSubmission);
 buildingCheckbox.addEventListener('input', toggleBuildingNumberVisibility);
 closeMenuButton.addEventListener('click', closeOrderMenu);
 
+allMeals.forEach(meal => {
+    meal.addEventListener('click', handleMealClick)
+});
+
+function handleMealClick() {
+    let mealId = this.getAttribute('data-meal');
+
+    if (chosenMeals.indexOf(mealId) === -1) {
+        addToChosenMeals(mealId);
+    } else {
+        removeFromChosenMeals(mealId);
+    }
+}
+
+function addToChosenMeals(mealId) {
+    let meal = document.querySelector(`.meal[data-meal="${mealId}"]`);
+
+    chosenMeals.push(mealId);
+    meal.classList.add('chosen');
+
+    updateLastPage();
+}
+
+function removeFromChosenMeals(mealId) {
+    let meal = document.querySelector(`.meal[data-meal="${mealId}"]`);
+
+    chosenMeals.splice(chosenMeals.indexOf('' + mealId), 1);
+    meal.classList.remove('chosen');
+
+    updateLastPage();
+}
+
+function updateLastPage() {
+    let chosenMealsContainer = document.querySelector('ul#order-items'),
+        totalPrice = 0,
+        totalPriceElement = document.querySelector('.total-price .price');
+
+    chosenMealsContainer.innerHTML = '';
+    totalPriceElement.innerHTML = '';
+    console.log(chosenMeals);
+
+    chosenMeals.forEach(mealId => {
+        totalPrice += parseFloat(mealsData[mealId-1].price);
+
+        chosenMealsContainer.innerHTML += `
+            <li>
+                ${mealsData[mealId-1].name} ( $ ${mealsData[mealId-1].price.toFixed(2)} )
+                <button class="remove" onclick="removeFromChosenMeals(${mealId})">Izbaci</button>
+            </li>
+        `;
+    });
+
+    console.log("When all is done" + totalPrice);
+
+    totalPriceElement.innerHTML += totalPrice.toFixed(2);
+}
+
+function openOrderForm() {
+    let orderForm = document.querySelector('.order-modal'),
+        menu = document.querySelector('.menu');
+
+    if (chosenMeals.length) {
+        orderForm.classList.add('active');
+        menu.classList.add('finished');
+    } else {
+        alert('You need to select some food...')
+    }
+}
+
 function closeOrderMenu() {
-    let orderModal = document.querySelector('.order-modal');
+    let orderModal = document.querySelector('.order-modal'),
+        menu = document.querySelector('.menu');
 
     orderModal.classList.remove('active');
+    menu.classList.remove('finished')
 }
 
 function toggleBuildingNumberVisibility() {
@@ -28,11 +153,11 @@ function toggleBuildingNumberVisibility() {
 function processOrderSubmission(event) {
     event.preventDefault();
 
-    let validationResult = validateForm();
+    alert("Porudžbina poslata! Bićete prebačeni na početnu stranicu za nekoliko sekundi.");
 
-    if (validationResult) {
-        window.location.href = "helper/order-complete.html";
-    }
+    setTimeout(() => {
+        window.location.href = "../index.html";
+    }, 2000);
 }
 
 function validateForm() {
